6cb9e0d78d404203340a4adef220c29333ff76af
mona <59416038+meowora@users.noreply.github.com>
2025-04-03 10:21:26 +0200

    add color component

--- .github/scripts/parsers/1_21_4/items1214.mjs
+++ .github/scripts/parsers/1_21_4/items1214.mjs
@@ -39,7 +39,8 @@ export const Items1214 = {
                             value: item.nbt.SkullOwner.Properties.textures[0].Value
                         }
                     ]
-                } : undefined
+                } : undefined,
+                'minecraft:dyed_color': item.nbt.display.color,
             }
         });
     },
